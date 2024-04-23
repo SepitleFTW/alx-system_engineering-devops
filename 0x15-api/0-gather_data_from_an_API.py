68e7a940f99d9a0d02be6315e986966d9cb99aa1
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-23 15:02:27 +0200

    ...

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 """
-returns list for employee ID given
+Returns to-do list information for a given employee ID.
+
+This script takes an employee ID as a command-line argument and fetches
+the corresponding user information and to-do list from the JSONPlaceholder API.
+It then prints the tasks completed by the employee.
 """
 
-
-import request
+import requests
 import sys
 
 
 if __name__ == "__main__":
-    # url for JSONplaceholder api
-    url = 'https://jsonplaceholder.typicode.com/'
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    # employees information by using employee ID
-    employee_id = sys.rgv[1]
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    # get to do list for the employee
+    # Get the to-do list for the employee using the provided employee ID
     params = {"userId": employee_id}
     todos = requests.get(url + "todos", params).json()
 
-    # filter the tasks and then count them up
+    # Filter completed tasks and count them
     completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    # print name and number of completed tasks
+    # Print the employee's name and the number of completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
 
-    # print out tasks that are done with indent
+    # Print the completed tasks one by one with indentation
     [print("\t {}".format(complete)) for complete in completed]
